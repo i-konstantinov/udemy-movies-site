528b21fc9b5eb5448b0794b52d9f1223a1d712c0
ivan-k <ivankonstantwork@gmail.com>
2021-09-22 19:10:49 +0300

    Added Category filter

--- IMDB_clone/IMDB_clone/movie/urls.py
+++ IMDB_clone/IMDB_clone/movie/urls.py
@@ -5,4 +5,5 @@ from IMDB_clone.movie import views
 urlpatterns = [
     path('', views.ListMovies.as_view(), name='list movies'),
     path('details/<int:pk>', views.MovieDetails.as_view(), name='movie details'),
+    path('category/', views.MovieCategory.as_view(), name='movie category'),
 ]
--- IMDB_clone/IMDB_clone/movie/views.py
+++ IMDB_clone/IMDB_clone/movie/views.py
@@ -1,21 +1,14 @@
-from django.shortcuts import render
+
 from django.views.generic import ListView, DetailView
 
 from IMDB_clone.movie.models import Movie, MovieLinks
-
-
-# def index(request):
-#     context = {
-#         'movies': Movie.objects.all(),
-#     }
-#     return render(request, 'list_movies.html', context)
 
 
 class ListMovies(ListView):
     model = Movie
     template_name = 'list_movies.html'
     context_object_name = 'movies'
-    paginate_by = 1
+    paginate_by = 2
 
 
 class MovieDetails(DetailView):
@@ -31,4 +24,24 @@ class MovieDetails(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['links'] = MovieLinks.objects.filter(movie=self.get_object())
+        return context
+
+
+class MovieCategory(ListView):
+    model = Movie
+    template_name = 'list_movies.html'
+
+    def get_queryset(self):
+        search_query = self.request.GET.get('choice')
+        wanted_category = None
+        for cat in self.model.CATEGORY_CHOICES:
+            if cat[1] == search_query:
+                wanted_category = cat[0]
+                break
+        movies = Movie.objects.filter(category=wanted_category)
+        return movies
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['movies'] = self.get_queryset()
         return context
